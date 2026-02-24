c911dc780d9a7b41058c78af4d9a965ce49440be
Stergios Papadopoulos <stevepd3333@gmail.com>
2026-02-24 22:39:47 +0200

    Prepared for the deployement

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,24 +7,34 @@ import authRouter from "./routes/auth_routes.js";
 import cookieParser from "cookie-parser"
 import { isAuth } from "./middleware/auth_middleware.js";
 import compression from "compression"
+import path from "path"
 
 const app = express()
+const __dirname = path.resolve()
 
 app.use(compression())
 
-// app.use(cors({origin: 'http://localhost:5173',
-//     credentials: true}));
+if(process.env.NODE_ENV !== "production") {
+    app.use(cors({origin: 'http://localhost:5173',
+    credentials: true}));
+}
+
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(limiter)
 
-app.use(express.static(path.join(__dirname, "public")));
-
 app.use("/api/notes", isAuth);
 app.use("/api/notes", notesRouter);
 
 app.use("/api", authRouter);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get(/(.*)/, (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 app.listen(5000, () => {
     connectDb()
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,16 +1,14 @@
 import axios from "axios";
 
- const api = axios.create({
-    baseURL: import.meta.env.MODE === 'development' ? 
-    'http://localhost:5000/api/notes' :
-    '/api',
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api"
+
+const api = axios.create({
+    baseURL: `${BASE_URL}/notes`,
     withCredentials: true
 });
 
 export const authapi = axios.create({
-    baseURL: import.meta.env.MODE === 'development' ? 
-    'http://localhost:5000/api/' :
-    '/api',
+    baseURL: BASE_URL,
     withCredentials: true
 });
 
