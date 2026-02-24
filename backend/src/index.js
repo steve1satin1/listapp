aea72522c9db00907146a9c4708d8376bdbe73ab
Stergios Papadopoulos <stevepd3333@gmail.com>
2026-02-24 23:54:21 +0200

    file error at deploying5

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,9 +31,9 @@ app.use("/api/notes", notesRouter);
 app.use("/api", authRouter);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "./frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
     app.get(/(.*)/, (req, res) => {
-        res.sendFile(path.join(__dirname, "./frontend", "dist", "index.html"))
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
     })
 }
 
